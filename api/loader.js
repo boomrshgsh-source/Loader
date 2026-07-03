9ff633561291b3b265deb0dbbb83594ab8d5a746
boomrshgsh-source <boomrshgsh@gmail.com>
2026-07-03 10:45:10 +0700

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -1,1 +1,21 @@
+import fs from "fs";
+import path from "path";
 
+export default function handler(req, res) {
+    const { script } = req.query;
+
+    if (!script) {
+        return res.status(400).send("Missing script");
+    }
+
+    const file = path.join(process.cwd(), "scripts", `${script}.lua`);
+
+    if (!fs.existsSync(file)) {
+        return res.status(404).send("Script not found");
+    }
+
+    const code = fs.readFileSync(file, "utf8");
+
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(code);
+}
