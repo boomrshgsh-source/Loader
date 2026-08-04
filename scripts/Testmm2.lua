5f7743ee3d0fa8354b94d7aafd1b87357de24bec
boomrshgsh-source <boomrshgsh@gmail.com>
2026-08-04 09:39:22 +0700

    Update Testmm2.lua

--- scripts/Testmm2.lua
+++ scripts/Testmm2.lua
@@ -1,1 +1,20 @@
+local Player = gsme:Getservice("Players").LocalPlayer
 
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local R = Character:WaitForChild("HumanoidRootPart")
+
+local autofarm = true
+
+local function findCoin()
+  if not autofarm then
+    return
+  end
+  local coin = workspace.Factory.CoinContainer
+
+  for _, v in ipirs(coin:GetChildren()) do
+    if v isA("Part") and v.Name == "Coin_Server" then
+      return v
+    end
+  end
+end
+whlie task.wait().
